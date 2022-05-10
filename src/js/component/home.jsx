0ba31e538ef79007ae599b078ffdab9eaf4a1c5b
YadiBelly <Yadisabell96@gmail.com>
2022-05-10 00:08:53 +0000

    updated

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Navbar from "./Navbar.jsx";
+import Jumbotron from "./Jumbotron.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,8 +8,11 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export const Home = () => {
 	return (
-		<div class="vw-100 vh-100">
-			<div class="w-100 h-25 bg-dark">text</div>
+		<div>
+			<Navbar />
+			<Jumbotron />
 		</div>
 	);
 };
+
+export default Home;
